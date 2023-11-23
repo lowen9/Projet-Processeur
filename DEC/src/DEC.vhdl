229ae522951c027a5ea692036c9ff925d5cd0994
Liu Owen - Linux <lowen999999@gmail.com>
2023-11-23 11:01:43 +0100

    Avancement sur DEC

--- DEC/src/DEC.vhdl
+++ DEC/src/DEC.vhdl
@@ -13,17 +13,10 @@ entity DEC is
 
 	    -- Decod to mem via exec
 			dec_mem_data	: out Std_Logic_Vector(31 downto 0); -- data to MEM
-<<<<<<< Updated upstream
-			dec_mem_dest	: out Std_Logic_Vector(3 downto 0);
-			dec_pre_index 	: out Std_logic;
-
-			dec_mem_lw		: out Std_Logic;
-=======
 			dec_mem_dest	: out Std_Logic_Vector(3 downto 0);  -- Registre d'écriture de Mem
 			dec_pre_index 	: out Std_logic;                     -- P = 1 calcul de l'adresse par exec sinon non
 
 			dec_mem_lw		: out Std_Logic; 
->>>>>>> Stashed changes
 			dec_mem_lb		: out Std_Logic;
 			dec_mem_sw		: out Std_Logic;
 			dec_mem_sb		: out Std_Logic;
@@ -87,19 +80,6 @@ entity DEC is
     
 end entity;
 
-<<<<<<< Updated upstream
-architecture ARCHI of DEC is 
-signal cond : std_logic;
-signal condv : std_logic;
-signal operv : std_logic;
-
-signal regop_t : std_logic;
-signal mult_t : std_logic;
-signal swap_t : std_logic;
-signal trans_t : std_logic;
-signal mtrans_t : std_logic;
-signal branch_t : std_logic;
-=======
 architecture ARCHI of DEC is
 --Précidat 
 signal cond : std_logic;  --fait 
@@ -117,7 +97,6 @@ signal mtrans_t : std_logic;
 signal branch_t : std_logic;
 
 --Traitement de données
->>>>>>> Stashed changes
 signal and_i: std_logic;
 signal eor_i : std_logic;
 signal sub_i : std_logic;
@@ -187,168 +166,6 @@ signal dec2if_push : std_logic;
 signal dec2exe_push : std_logic;
 signal if2dec_pop : std_logic;
 
-<<<<<<< Updated upstream
-begin 
-        component REG 
-        port(
-            -- Write Port 1 prioritaire
-                wdata1		    : in Std_Logic_Vector(31 downto 0);
-                wadr1			: in Std_Logic_Vector(3 downto 0);
-                wen1			: in Std_Logic;
-
-            -- Write Port 2 non prioritaire
-                wdata2		    : in Std_Logic_Vector(31 downto 0);
-                wadr2			: in Std_Logic_Vector(3 downto 0);
-                wen2			: in Std_Logic;
-
-            -- Write CSPR Port
-                wcry			: in Std_Logic;
-                wzero			: in Std_Logic;
-                wneg			: in Std_Logic;
-                wovr			: in Std_Logic;
-                cspr_wb		    : in Std_Logic;
-                
-            -- Read Port 1 32 bits
-                reg_rd1		    : out Std_Logic_Vector(31 downto 0);
-                radr1			: in Std_Logic_Vector(3 downto 0);
-                reg_v1		    : out Std_Logic;
-
-            -- Read Port 2 32 bits
-                reg_rd2		    : out Std_Logic_Vector(31 downto 0);
-                radr2			: in Std_Logic_Vector(3 downto 0);
-                reg_v2		    : out Std_Logic;
-
-            -- Read Port 3 32 bits
-                reg_rd3		    : out Std_Logic_Vector(31 downto 0);
-                radr3			: in Std_Logic_Vector(3 downto 0);
-                reg_v3		    : out Std_Logic;
-
-            -- read CSPR Port
-                reg_cry		    : out Std_Logic;
-                reg_zero		: out Std_Logic;
-                reg_neg		    : out Std_Logic;
-                reg_cznv		: out Std_Logic;
-                reg_ovr		    : out Std_Logic;
-                reg_vv		    : out Std_Logic;
-                
-            -- Invalidate Port 
-                inval_adr1	    : in Std_Logic_Vector(3 downto 0);
-                inval1		    : in Std_Logic;
-
-                inval_adr2	    : in Std_Logic_Vector(3 downto 0);
-                inval2		    : in Std_Logic;
-
-                inval_czn	    : in Std_Logic;
-                inval_ovr	    : in Std_Logic;
-
-            -- PC
-                reg_pc		    : out Std_Logic_Vector(31 downto 0);
-                reg_pcv		    : out Std_Logic;
-                inc_pc		    : in Std_Logic;
-            
-            -- global interface
-                ck		        : in Std_Logic;
-                reset_n		    : in Std_Logic;
-                vdd			    : in bit;
-                vss			    : in bit);
-        end component; 
-        if (reg_cznv='1' and reg_vv= '1') then
-            condv <= '1';
-        end if; 
-        when (if_ir(31 downto 28)) = "0000" => --cond EQ 
-            if reg_zero='1' then 
-                cond<= '1'; 
-            else 
-                cond<='0';
-            end if; 
-        when (if_ir(31 downto 28)) = "0001" => --cond ne
-            if reg_zero='0' then 
-                cond<= '1'; 
-            else 
-                cond<='0';
-            end if; 
-        when (if_ir(31 downto 28)) = "0010" => --cond C=1
-            if  reg_cry='1' then 
-                cond= '1'; 
-            else 
-                cond='0';
-            end if; 
-        when (if_ir(31 downto 28)) = "0011" => --cond C=0 
-            if reg_cry ='0' then 
-                cond<= '1'; 
-            else 
-                cond<='0';
-            end if; 
-        when (if_ir(31 downto 28)) = "0100" => -- cond N=1
-            if reg_neg ='1' then 
-                cond<= '1'; 
-            else 
-                cond<='0';
-            end if ; 
-        when (if_ir(31 downto 28)) = "0101" =>--cond N=0 
-            if reg_neg= '0 ' then 
-                cond<= '1'; 
-            else 
-                cond<='0';
-            end if; 
-        when (if_ir(31 downto 28)) = "0110" => --cond ovr=1
-            if reg_ovr='1' then 
-                cond<= '1'; 
-            else 
-                cond<='0';
-            end if; 
-        when (if_ir(31 downto 28)) = "0111" => --cond ovr=0 
-            if reg_ovr='0' then 
-                cond<= '1'; 
-            else 
-                cond<='0';
-            end if; 
-        when (if_ir(31 downto 28)) = "1000" => -- cond c=1 et Z=0 
-            if (reg_cry='1' and reg_zero='0') then 
-                cond<= '1'; 
-            else 
-                cond<='0';
-            end if; 
-        when (if_ir(31 downto 28)) = "1001" => -- cond c=0 ou Z=1 
-            if (reg_cry='0' or reg_zero='1') then 
-                cond<= '1'; 
-            else 
-                cond<='0';
-            end if; 
-        when (if_ir(31 downto 28)) = "1010" => --conf sup ou egal 
-            if reg_zero='1' then 
-                cond<= '1'; 
-            else 
-                cond<='0';
-            end if; 
-        when (if_ir(31 downto 28)) = "1011" => --cond strictement inf 
-            if reg_zero='1' then 
-                cond<= '1'; 
-            else 
-                cond<='0';
-            end if;
-        when (if_ir(31 downto 28)) = "1100" => ---cond stric sup 
-            if reg_zero='1' then 
-                cond<= '1'; 
-            else 
-                cond<='0';
-            end if;
-        when (if_ir(31 downto 28)) = "1101" => --inf ou egal 
-            if reg_zero='1' then 
-                cond<= '1'; 
-            else 
-                cond<='0';
-            end if;
-        when (if_ir(31 downto 28)) = "1110" => --toujours
-                cond <= '1'; 
-        when (if_ir(31 downto 28)) = "1111" => --reserve 
-                cond <= '0'; 
-        
-                
-
-
- 
-=======
 --Signaux interne de REG
 signal radr1, radr2, radr3 : std_logic_vector(3 downto 0);
 signal shift_val_nt : std_logic_vector(31 downto 0);
@@ -448,6 +265,5 @@ begin
 
         if_ir(27 downto )
 
->>>>>>> Stashed changes
 
 end ARCHI
