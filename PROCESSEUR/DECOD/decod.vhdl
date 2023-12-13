812f6212a5ead8b421196c88f567112b1456c829
CameliaBouras <cameliabouras29@gmail.com>
2023-12-13 14:02:15 +0100

    commencer link

--- PROCESSEUR/DECOD/decod.vhdl
+++ PROCESSEUR/DECOD/decod.vhdl
@@ -525,8 +525,8 @@ begin
 
 -- decod regop opcode
 
-	and_i <= '1' when regop_t = '1' and if_ir(24 downto 21) = X"0" else '0';
-	eor_i <= '1' when (regop_t & if_ir(24 downto 21) = "10001")    else '0';
+  and_i <= '1' when regop_t = '1' and if_ir(24 downto 21) = X"0" else '0';
+  eor_i <= '1' when (regop_t & if_ir(24 downto 21) = "10001")    else '0';
   sub_i <= '1' when (regop_t & if_ir(24 downto 21) = "10010")    else '0';
   rsb_i <= '1' when (regop_t & if_ir(24 downto 21) = "10011")    else '0';
   add_i <= '1' when (regop_t & if_ir(24 downto 21) = "10100")    else '0';
@@ -549,7 +549,11 @@ begin
 -- mtrans instruction
 
 -- branch instruction
+-- Branch and link 
+bl_i <= '1' when (branch_t & if_ir(24)="11"); 
 
+
+--blink <= '1' when (bl_i & (ecriture finis dans R14))
 -- Decode interface operands
 	op1 <=	reg_pc		  	when (branch_t = '1'	         	) else
 	        (others=>'0') when (mov_i = '1' or mvn_i = '1') else
@@ -593,15 +597,17 @@ begin
 -- Reg Invalid
 
 	inval_exe_adr <= X"F"                when (branch_t = '1') else
-		             	 if_ir(19 downto 16) when (mult_t   = '1') else
-									 if_ir(15 downto 12);
+		             if_ir(19 downto 16) when (mult_t   = '1') else
+					 if_ir(15 downto 12) else 
+					 X"E" when (bl_i = '1');
 
 	inval_exe <= '0' when (branch_t                  = '1' or 
 	                      (regop_t and (not(tst_i) or not(teq_i) or 
-																			not(cmp_i) or not(cmn_i))) 
-										             									 = '1' or 
-						  					 mult_t                    = '1' or
-						  					 alu_wb                    = '1') else 
+										not(cmp_i) or not(cmn_i))) 
+							 						  = '1' or 
+							mult_t                    = '1' or
+						  	alu_wb                    = '1' or
+							bl_i                      = '1'  )  else 
 				 			 '1';
 
 	inval_mem_adr <= if_ir(19 downto 16) when (alu_wb = '1') else
@@ -791,6 +797,9 @@ begin
 									 end if;
 
 		when LINK => next_state <= LINK;
+									 
+							
+								
 		
 		when MTRANS => next_state <= MTRANS;
 	end case;
